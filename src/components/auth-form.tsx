5d5498a6e3745d56c52384776e2f6d2fd4368f8b
Aarush-sharma <aarush.sharma.fpw@gmail.com>
2024-06-06 18:06:13 +0530

    resolved email issue

--- src/components/auth-form.tsx
+++ src/components/auth-form.tsx
@@ -23,7 +23,7 @@ export default function AuthenticationPage() {
           email: email,
         },
       });
-      localStorage.setItem("email",email)
+      localStorage.setItem("email", email);
       console.log(res.data);
       setIsLoading(false);
       toast({
@@ -67,29 +67,13 @@ export default function AuthenticationPage() {
                     Sign Up with Email
                   </Button>
                 </form>
-
-                <div className="relative">
-                  <div className="absolute inset-0 flex items-center">
-                    <span className="w-full border-t" />
-                  </div>
-                  <div className="relative flex justify-center text-xs uppercase">
-                    <span className="bg-background px-2 text-muted-foreground">
-                      Or continue with
-                    </span>
-                  </div>
-                </div>
-                <Button variant="outline" type="button" disabled={isLoading}>
-                  {isLoading ? (
-                    <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
-                  ) : (
-                    <Icons.google className="mr-2 h-4 w-4" />
-                  )}{" "}
-                  Google
-                </Button>
               </div>
               <div className="flex justify-center ">
                 <p className=" pt-1">Already have a account?</p>
-                <a href="/log-in" className="mt-1 ml-1 focus:text-gray-200 hover:decoration-muted">
+                <a
+                  href="/log-in"
+                  className="mt-1 ml-1 focus:text-gray-200 hover:decoration-muted"
+                >
                   login
                 </a>
               </div>
